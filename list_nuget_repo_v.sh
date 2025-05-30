3d42cf1987563c4fd32bc64cb63d3c61a592a9c2
arroyox <adrian.arroyo@gmail.com>
2025-05-30 12:20:36 -0700

    Update list_nuget_repo_v.sh

--- list_nuget_repo_v.sh
+++ list_nuget_repo_v.sh
@@ -2,12 +2,13 @@
 
 read -p "Enter NuGet package name (e.g., Newtonsoft.Json): " PKG
 
-OUT="nugetversion.txt"
+OUT="version_nuget.txt"
 : > "$OUT"
 
 VERS=$(curl -s "https://api.nuget.org/v3-flatcontainer/${PKG,,}/index.json" | jq -r '.versions[]')
 for VER in $VERS; do
-    echo -e "${VER}\thttps://www.nuget.org/packages/${PKG}/${VER}" >> "$OUT"
+    echo -e "https://www.nuget.org/packages/${PKG}/${VER}" >> "$OUT"
 done
 
 echo "Saved versions and URLs to $OUT"
+root@W-5CG4331HH8:~#
