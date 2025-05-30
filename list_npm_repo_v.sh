6348981eb4582923f841105e1262ec44b871d3c2
arroyox <adrian.arroyo@gmail.com>
2025-05-30 12:20:10 -0700

    Update list_npm_repo_v.sh

--- list_npm_repo_v.sh
+++ list_npm_repo_v.sh
@@ -2,12 +2,12 @@
 
 read -p "Enter npm package name (e.g., lodash): " PKG
 
-OUT="npmversion.txt"
+OUT="version_npm.txt"
 : > "$OUT"
 
 VERS=$(curl -s "https://registry.npmjs.org/${PKG}" | jq -r '.versions | keys[]')
 for VER in $VERS; do
-    echo -e "${VER}\thttps://www.npmjs.com/package/${PKG}/v/${VER}" >> "$OUT"
+    echo -e "https://www.npmjs.com/package/${PKG}/v/${VER}" >> "$OUT"
 done
 
 echo "Saved versions and URLs to $OUT"
